ce54e9590877d4d9114db1d222bf04d3f584c753
Tainan21 <tainancmoraes@gmail.com>
2023-02-14 16:46:57 -0300

    alter public

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 /**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
+ * WEBSITE:
+ * TWITTER:
+ * FACEBOOK:
+ * GITHUB:
  */
 
 (function ($) {

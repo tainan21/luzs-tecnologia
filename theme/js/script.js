ab653e37e1dacf0241c331b6b1a996acd8e415c7
Tainan21 <tainancmoraes@gmail.com>
2023-03-13 20:53:04 -0300

    alter ponto

--- theme/js/script.js
+++ theme/js/script.js
@@ -170,6 +170,45 @@
       infinite: true,
       arrows: false,
       autoplay: true,
+      autoplaySpeed: 50000,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 0,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
+    });
+    
+    $('.product-slider').slick({
+      slidesToShow: 1,
+      infinite: true,
+      arrows: false,
+      autoplay: true,
       autoplaySpeed: 1000,
       dots: true,
       responsive: [
